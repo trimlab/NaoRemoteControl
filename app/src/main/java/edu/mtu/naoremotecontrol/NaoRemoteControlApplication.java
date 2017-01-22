c0325000f4c0c7f745958c729c87820f4c1304ae
ericmvasey <ericmvasey@comcast.net>
2017-01-22 16:34:39 -0500

    Testing with new library.

--- app/src/main/java/edu/mtu/naoremotecontrol/NaoRemoteControlApplication.java
+++ app/src/main/java/edu/mtu/naoremotecontrol/NaoRemoteControlApplication.java
@@ -3,17 +3,17 @@ package edu.mtu.naoremotecontrol;
 import android.app.Application;
 import android.support.v4.util.Pair;
 
-import com.aldebaran.qi.CallError;
-import com.aldebaran.qi.Session;
-import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
-import com.aldebaran.qi.helper.proxies.ALAutonomousLife;
-import com.aldebaran.qi.helper.proxies.ALBattery;
-import com.aldebaran.qi.helper.proxies.ALBodyTemperature;
-import com.aldebaran.qi.helper.proxies.ALMotion;
-import com.aldebaran.qi.helper.proxies.ALPreferences;
-import com.aldebaran.qi.helper.proxies.ALRobotPosture;
-import com.aldebaran.qi.helper.proxies.ALSystem;
-import com.aldebaran.qi.helper.proxies.ALTextToSpeech;
+import com.aldebaran.qimessaging.CallError;
+import com.aldebaran.qimessaging.Session;
+import com.aldebaran.qimessaging.helpers.al.ALAnimatedSpeech;
+import com.aldebaran.qimessaging.helpers.al.ALAutonomousLife;
+import com.aldebaran.qimessaging.helpers.al.ALBattery;
+import com.aldebaran.qimessaging.helpers.al.ALBodyTemperature;
+import com.aldebaran.qimessaging.helpers.al.ALMotion;
+import com.aldebaran.qimessaging.helpers.al.ALPreferences;
+import com.aldebaran.qimessaging.helpers.al.ALRobotPosture;
+import com.aldebaran.qimessaging.helpers.al.ALSystem;
+import com.aldebaran.qimessaging.helpers.al.ALTextToSpeech;
 
 import java.util.List;
 
